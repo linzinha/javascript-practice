3edac59c6320337eedabe10d2afa89411873fc6c
linzinha <45407371+linzinha@users.noreply.github.com>
2020-04-04 17:06:07 +0100

    Create iterators.js

---
+++ iterators.js
@@ -0,0 +1,11 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(fruit => {
+  if (fruit[0] === 'a'){
+    console.log('I want to eat an ' + fruit)
+  } else {
+  console.log('I want to eat a ' + fruit)
+  }
+});
+
