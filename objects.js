0e06dcc21031ff07e61e5d9793e5f27e6f0096bb
linzinha <45407371+linzinha@users.noreply.github.com>
2020-04-06 21:32:10 +0100

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,20 @@
+let cat = {
+  ears: 'perked',
+  eyes: 'wide',
+  status: 'alert',
+  whiskers: 'floofed',
+  bugsEaten: 6,
+  isHappy () {
+    console.log('Mew! Purrrrrrrr')
+  },
+  isHungry () {
+    console.log('MEOW MEOW MEOWMEOWMEOW MEOOOOOWWWWW')
+  },
+  isAngry () {
+    console.log('HISSS')
+  }
+};
+
+cat.isHappy()
+cat.isHungry()
+cat.isAngry()
