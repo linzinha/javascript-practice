4b84d48b425d2936b21a057a7606a56ab44794d8
linzinha <45407371+linzinha@users.noreply.github.com>
2020-04-06 23:52:00 +0100

    Update objects.js

--- objects.js
+++ objects.js
@@ -8,7 +8,68 @@ let cat = {
     console.log('Mew! Purrrrrrrr')
   },
   isHungry () {
+    console.log('MEOW ME// let spaceship = {
+//   'Fuel Type' : 'Turbo Fuel',
+//   homePlanet : 'Earth'
+// };
+
+// // Write your code below
+// let greenEnergy = ship => {
+//   ship['Fuel Type'] = 'avocado oil';
+// };
+
+// let remotelyDisable = ship => {
+//   ship.disabled = true;
+// };
+
+// greenEnergy(spaceship);
+// remotelyDisable(spaceship);
+// console.log(spaceship);
+
+let cat = {
+  ears: 'perked',
+  eyes: 'wide',
+  status: 'is Hungry',
+  whiskers: 'floofed',
+  bugsEaten: 6,
+  isHappy () {
+    console.log('Mew! Purrrrrrrr')
+  },
+  isHungry () {
     console.log('MEOW MEOW MEOWMEOWMEOW MEOOOOOWWWWW')
+  },
+  isAngry () {
+    console.log('HISSS')
+  }
+};
+
+
+
+
+
+
+let feedCat = cat => {
+  console.log("Feeding feline. Cat seems satisfied with food selection - this time.");
+  cat.status = 'is Happy';
+}
+
+let doesvomit = () => {
+  yesNo = Math.round(Math.random());
+  if (yesNo === 0) {
+    return "And it's your lucky day, because Cat doesn't immediate vomit it up on the one thing on the floor that is difficult to clean"
+  } else {
+    return "New status update. Stomach distress. Cat has thrown up lunch on that one piece of important paper that accidentally fell on the floor."
+  }
+}
+
+
+console.log("Assessing cat status. Cat " + cat.status + ".");
+cat.isHungry()
+feedCat(cat);
+console.log("Confirming status update. Cat " + cat.status) + ".";
+cat.isHappy()
+console.log(doesvomit())
+OW MEOWMEOWMEOW MEOOOOOWWWWW')
   },
   isAngry () {
     console.log('HISSS')
