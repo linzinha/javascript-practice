6e012fe31e791aedd619130e11e668c2337507fa
linzinha <45407371+linzinha@users.noreply.github.com>
2020-04-06 19:00:13 +0100

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -71,3 +71,25 @@ const newSum = newNumbers.reduce(function(accumulator, currentValue){
 }, 10);
 
 console.log(newSum)
+
+
+
+//using some, filter, every
+//
+///////////////////////////////////////////////////
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// Something is missing in the method call below
+
+console.log(words.some((word) => {
+  return word.length < 6;
+}));
+
+// Use filter to create a new array
+const interestingWords = words.filter(word => word.length > 5);
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+
+console.log(interestingWords.every((word) => {words.filter(word => word.length > 5) } ));
